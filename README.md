a9f2d3008bc46dee48abe07618cff3e6d6237148
Jan Valkenburg <jan.valkenburg@gmail.com>
2022-11-17 11:10:59 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Frontendkit buildHeaderStylesheet
+Node.js module to generate a html file with a list of all used css files within the project.
+
+## install 
+copy the stylesheet.json file to the root of the project and set it up in the way that is required for your project.
+
---
+++ index.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const fs = require('fs');
+const settingsFile = 'stylesheets.json';
+function priocopyfiles() {
+    var files = [];
+    let html = '';
+    if (fs.existsSync(settingsFile) === false) {
+        console.log(settingsFile + ' NOT FOUND!')
+        return false;
+    }
+    const settings = JSON.parse(fs.readFileSync(settingsFile));
+    var sourceFolders = settings.folders.reverse();
+    for (let file of settings.files) {
+        for (let sourceFolder of sourceFolders) {
+            if (fs.existsSync(settings.rootSource + sourceFolder + file)) {
+                html += "\n<link rel=\"stylesheet\" href='" + sourceFolder + file + "'>";
+                break;
+            }
+        }
+    }
+    fs.writeFileSync(settings.outputFile, html);
+}
+priocopyfiles();
